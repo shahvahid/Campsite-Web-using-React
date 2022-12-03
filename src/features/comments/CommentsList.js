f35e15704753e8d4e188fac16c029109186e5a50
Vahid Shah Hosseini <shah.vahid@yahoo.com>
2022-12-03 11:49:55 -0600

    first commit

---
+++ src/features/comments/CommentsList.js
@@ -0,0 +1,38 @@
+import { useSelector } from 'react-redux';
+import { Col } from 'reactstrap';
+import Comment from './Comment';
+import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm'
+import Error from '../../components/Error';
+import Loading  from'../../components/Loading';
+
+const CommentsList = ({ campsiteId }) => {
+    console.log('campsiteid');    
+    console.log(campsiteId);
+    const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
+
+    if (comments && comments.length > 0) {
+        return isLoading ? (
+            <Loading/>
+        ) : errMsg ? (
+            <Error errMsg={errMsg} />
+        ) : (
+            <Col md='5' className='m-1'>
+                <h4>Comments</h4>
+                {comments.map((comment) => {
+                    return <Comment key={comment.id} comment={comment} />;
+                })}
+                <CommentForm campsiteId={campsiteId}></CommentForm>
+            </Col>
+        );
+    }
+    return (
+        <Col md='5' className='m-1'>
+            There are no comments for this campsite yet.
+        </Col>
+    );
+};
+
+export default CommentsList;
